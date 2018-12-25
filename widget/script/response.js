08ae5439a5f86ccb06e2a871ee213dccd1abfd52
Sarah <zhangqianjun521@163.com>
2018-12-25 10:43:51 +0800

    #ufu

--- widget/script/response.js
+++
@@ -1,6 +0,0 @@
-(function (doc, win) {
-  var docEl = doc.documentElement
-  var screenWidth = docEl.clientWidth
-  // var screenWidth = win.screen.width
-  docEl.style.fontSize = 20 * (screenWidth / 750) + 'px'
-})(document, window)
