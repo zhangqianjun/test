a9482ca191631c9684e72643559485234f446796
Heziming <294139237@qq.com>
2019-01-10 18:11:04 +0800

    ttest

--- widget/src/pages/index/app.vue
+++ widget/src/pages/index/app.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <!-- <ul v-if="dataHasLoaded">
+  <div>
+    <!-- <ul v-if="dataHasLoaded">
             <li>引擎版本信息: {{ver}}</li>
             <li>系统类型: {{sType}}</li>
             <li>系统版本: {{sVer}}</li>
@@ -15,33 +15,57 @@
             <li>子窗口宽度: {{frameWidth}}</li>
             <li>子窗口高度: {{frameHeight}}</li>
         </ul> -->
-        <div class="content">
-        <router-view></router-view>
-        </div>
+    <div class="content">
+      <div>123</div>
+      <div>{{test}}</div>
+      <router-view></router-view>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                dataHasLoaded: false
-            }
-        },
-        created() {
-            // this.ver = api.version;
-            // this.sType = api.systemType;
-            // this.sVer = api.systemVersion;
-            // this.id = api.deviceId;
-            // this.model = api.deviceModel;
-            // this.name = api.deviceName;
-            // this.cType = api.connectionType;
-            // this.winName = api.winName;
-            // this.winWidth = api.winWidth;
-            // this.winHeight = api.winHeight;
-            // this.frameName = api.frameName || '';
-            // this.frameWidth = api.frameWidth || '';
-            // this.frameHeight = api.frameHeight || '';
-            // this.dataHasLoaded = true;
-        }
+export default {
+  data() {
+    return {
+      dataHasLoaded: false,
+      test: 222331
     }
+  },
+  created() {
+    var ajpush = api.require('ajpush');
+    ajpush.init(function (ret, err) {
+      if (ret && ret.status) {
+        // alert('操作成功!');
+      } else {
+        alert('操作失败!');
+      }
+    });
+    // api.addEventListener({name:'appintent'}, function(ret,err) {
+    //     alert('通知被点击，收到数据：\n' + JSON.stringify(ret));//监听通知被点击后收到的数据
+    //     if(ret && ret.appParam.ajpush){
+    //     }
+    //    })
+    ajpush.setListener((ret) => {
+      //   alert(123456)
+      var sw = api.require('screenWake');
+      sw.addScreenWake();
+      console.log(JSON.stringify(ret))
+      api.notification({
+        vibrate: [100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500],
+        // light: true,
+        sound: 'widget://res/music.mp3',
+        notify: {
+          title: '测试',
+          content: '测试试试',
+          extra: 'fdsfds',
+          updateCurrent: true
+        }
+      })
+      var id = ret.id;
+      var title = ret.title;
+      var content = ret.content;
+      var extra = ret.extra;
+      this.test = content;
+    });
+  }
+}
 </script>
